0b9665521f55b3a963021ddd3351e507bacde956
Eric Corson <emccorson@gmail.com>
2016-11-18 23:44:03 +0000

    Poll for beacons and log the number of beacons we can see

--- app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
+++ app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
@@ -1,13 +1,57 @@
 package com.wellsetaboutye.glesga;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    protected static final String TAG = "MainActivity";
+    private BeaconManager kevinBeaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "fine runner beans");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kevinBeaconManager = BeaconManager.getInstanceForApplication(this);
+        kevinBeaconManager.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        kevinBeaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Log.i(TAG, "working a bit");
+
+        kevinBeaconManager.addRangeNotifier(new RangeNotifier() {
+
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                long time = System.nanoTime();
+                Log.i(TAG, "working seriously " + collection.size());
+                for (Beacon b : collection)
+                    Log.i(TAG, time + " " + b.getServiceUuid());
+            }
+        });
+
+        try {
+            kevinBeaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            Log.i(TAG, "something has gone horribly wrong here");
+        }
     }
 }
