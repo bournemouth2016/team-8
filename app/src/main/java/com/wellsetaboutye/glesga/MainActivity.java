b353c559a085e746a60931bf1d1e9d657ccaded7
Eric Corson <emccorson@gmail.com>
2016-11-19 10:32:18 +0000

    Add speaking to the bad numbers app

--- app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
+++ app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
@@ -1,10 +1,9 @@
 package com.wellsetaboutye.glesga;
 
-import android.content.Context;
-import android.os.RemoteException;
-import android.os.Vibrator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -20,25 +19,26 @@ import java.util.Collection;
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
     private static final String BEACON_NAME = "MiniBeacon_05440";
-    private static final int MAXISH = 6; // some number bigger than the distance will ever be
 
     protected static final String TAG = "MainActivity";
     private BeaconManager kevinBeaconManager;
     private Beacon tracked = null;
-    private Vibrator vibrator;
-    private TextView distanceTextView = null;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "fine runner beans");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         kevinBeaconManager = BeaconManager.getInstanceForApplication(this);
         kevinBeaconManager.bind(this);
         kevinBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-        vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
-
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                ; // have a party
+            }
+        });
 
     }
 
@@ -50,25 +50,23 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
     @Override
     public void onBeaconServiceConnect() {
-        Log.i(TAG, "working a bit");
 
         kevinBeaconManager.addRangeNotifier(new RangeNotifier() {
 
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
-                long time = System.nanoTime();
-                Log.i(TAG, "working seriously " + collection.size());
                 for (Beacon b : collection) {
-                    //Log.i(TAG, time + " " + b.getBluetoothName() + " " + b.getDistance() + " " + b.getTxPower());
                     if (b.getBluetoothName().equals(BEACON_NAME)) {
                         tracked = b;
                     }
                 }
                 if (tracked != null) {
                     Log.i(TAG, "tracked @ " + tracked.getDistance());
+                    final String niceDistanceString = String.format("%.2f", tracked.getDistance());
+                    tts.speak(niceDistanceString, TextToSpeech.QUEUE_FLUSH, null);
                     runOnUiThread(new Runnable() {
                         public void run() {
-                            ((TextView) findViewById(R.id.distanceTextView)).setText(String.format("%.2f", tracked.getDistance()));
+                            ((TextView) findViewById(R.id.distanceTextView)).setText(niceDistanceString);
                         }
                     });
                 }
