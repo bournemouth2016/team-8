997806e11cfe1c0e117b469ede22342a97907aeb
Stewart Hamilton <stewart.w.hamilton@gmail.com>
2016-11-19 03:56:55 +0000

    steps

--- app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
+++ app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
@@ -1,13 +1,62 @@
 package com.wellsetaboutye.glesga;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+
+    private Sensor mStepDetectorSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
+        Log.i("S", "Step Detect");
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+            if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            Log.i("S", "Step Detector Detected : " + value);
+        }
+
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
