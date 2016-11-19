b6298d5f9274a3202a1347e803a8ac3a35ca034a
Eric Corson <emccorson@gmail.com>
2016-11-19 05:43:07 +0000

    Vibrate the closer you are to 05440 (allegedly)

--- app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
+++ app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wellsetaboutye.glesga;
 
+import android.content.Context;
 import android.os.RemoteException;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,8 +18,13 @@ import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
+    private static final String BEACON_NAME = "MiniBeacon_05440";
+    private static final int MAXISH = 6; // some number bigger than the distance will ever be
+
     protected static final String TAG = "MainActivity";
     private BeaconManager kevinBeaconManager;
+    private Beacon tracked = null;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         kevinBeaconManager = BeaconManager.getInstanceForApplication(this);
         kevinBeaconManager.bind(this);
         kevinBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @Override
@@ -46,7 +55,14 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
                 long time = System.nanoTime();
                 Log.i(TAG, "working seriously " + collection.size());
                 for (Beacon b : collection) {
-                    Log.i(TAG, time + " " + b.getManufacturer() + b.getBluetoothName() + b.getParserIdentifier());
+                    //Log.i(TAG, time + " " + b.getBluetoothName() + " " + b.getDistance() + " " + b.getTxPower());
+                    if (b.getBluetoothName().equals(BEACON_NAME)) {
+                        tracked = b;
+                    }
+                }
+                if (tracked != null) {
+                    Log.i(TAG, "tracked @ " + tracked.getDistance());
+                    vibrator.vibrate((long) (tracked.getDistance() > 1 ? 1000 / tracked.getDistance() : 1000));
                 }
             }
         });
