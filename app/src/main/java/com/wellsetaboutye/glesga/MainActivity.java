05ce74260c78ced114d3eb3e0776d9561c870147
Graham Fyfe <Graham Fyfe>
2016-11-19 00:39:17 +0000

    Add initial bluetooth stuff

--- app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
+++ app/src/main/java/com/wellsetaboutye/glesga/MainActivity.java
@@ -1,5 +1,7 @@
 package com.wellsetaboutye.glesga;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,35 +25,13 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         Log.i(TAG, "fine runner beans");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        kevinBeaconManager = BeaconManager.getInstanceForApplication(this);
-        kevinBeaconManager.bind(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        kevinBeaconManager.unbind(this);
-    }
-
-    @Override
-    public void onBeaconServiceConnect() {
-        Log.i(TAG, "working a bit");
-
-        kevinBeaconManager.addRangeNotifier(new RangeNotifier() {
-
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        adapter.startLeScan(new BluetoothAdapter.LeScanCallback() {
             @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
-                long time = System.nanoTime();
-                Log.i(TAG, "working seriously " + collection.size());
-                for (Beacon b : collection)
-                    Log.i(TAG, time + " " + b.getServiceUuid());
+            public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+                Log.d(TAG, bluetoothDevice.toString());
             }
-        });
+        })
 
-        try {
-            kevinBeaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {
-            Log.i(TAG, "something has gone horribly wrong here");
-        }
     }
 }
